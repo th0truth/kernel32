69e2649ac29da7e34a4368c9381abd5e87c2462d
qthetruth <shiftrams@gmail.com>
2025-07-11 20:24:04 +0300

    video memory

--- kernel.c
+++ kernel.c
@@ -10,27 +10,27 @@
 
 void clear_screen() {
   // Define the video memory variable that points to the address 0xb8000
-  char *vidptr = VIDEO_MEMORY;
+  char *video_memory = VIDEO_MEMORY;
   unsigned i = 0;
   
   // This loop that clears the screen
   // there are 25 lines each of 80 columns. Each element takes 2 bytes
   while (i < VGA_MEM_WIDTH * VGA_MEM_HEIGHT * 2) {
-    vidptr[i] = ' ';
-    vidptr[i+1] = WHITE_ON_BLACK;
+    video_memory[i] = ' ';
+    video_memory[i+1] = WHITE_ON_BLACK;
     i += 2;
   }
 }
 
 void putstr(const char *s) {
   // Define the video memory variable that points to the address 0xb8000
-  char *vidptr = VIDEO_MEMORY;
+  char *video_memory = VIDEO_MEMORY;
   unsigned i = 0, j = 0;
 
   // This loop writes the string to video memory
   while (s[j] != '\0') {
-    vidptr[i] = s[j];
-    vidptr[i+1] = WHITE_ON_BLACK;
+    video_memory[i] = s[j];
+    video_memory[i+1] = WHITE_ON_BLACK;
     i += 2;
     j++;
   }
